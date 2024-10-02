83c1b78b949191fe135c0bd933dd67e0094b8344
steff09 <steffany@glya.co>
2024-10-02 18:16:26 -0500

    initial commit

--- register-app/src/app/app.component.spec.ts
+++ register-app/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'registration-app' title`, () => {
+  it(`should have the 'register-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('registration-app');
+    expect(app.title).toEqual('register-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, registration-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, register-app');
   });
 });
